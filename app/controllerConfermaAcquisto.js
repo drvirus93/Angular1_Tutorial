74937cd4d0c93fa8dbb86dd6fe7a0e3d182e2a41
drvirus93 <drvirus93@hotmail.it>
2016-11-16 14:43:56 +0100

    Valorizzato array

--- app/controllerConfermaAcquisto.js
+++ app/controllerConfermaAcquisto.js
@@ -1,4 +1,11 @@
 angular.module('gemStore')
 .controller('ControllerConfermaAcquisto',function($rootScope,$scope){
 
+
+  $scope.confirmOrder = function() {
+
+  console.log($rootScope.user.length);
+  console.log($scope.user.length);
+  }
+
 });
